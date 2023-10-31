b90482ff8a9612ef6ab01d89d946a33d5e6f883a
Sam Morsics <sammorsics@MacBook-Pro.local>
2023-10-31 18:54:48 -0400

    Adding main method TEST

---
+++ main.py
@@ -0,0 +1,11 @@
+import decode as dec
+import encode as enc
+
+def main():
+    print(enc.encode("12345"))
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
